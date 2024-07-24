c0800abe15d0753d9f2f8742a6a5a0187bfe2a0f
Aleksa <bonixsrbija@gmail.com>
2024-07-24 23:03:56 +0200

    added nightslash

---
+++ Content/Items/Weapons/Nightslash.cs
@@ -0,0 +1,62 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using Paracosm.Content.Projectiles;
+using Terraria.DataStructures;
+using Paracosm.Content.Items.Materials;
+
+namespace Paracosm.Content.Items.Weapons
+{
+    public class Nightslash : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.damage = 15;
+            Item.DamageType = DamageClass.Melee;
+            Item.width = 50;
+            Item.height = 60;
+            Item.useTime = 10;
+            Item.useAnimation = 10;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.knockBack = 4;
+            Item.value = Item.buyPrice(gold: 10);
+            Item.rare = ItemRarityID.Green;
+            Item.UseSound = SoundID.Item1;
+            Item.autoReuse = true;
+            Item.useTurn = false;
+            Item.shoot = ModContent.ProjectileType<NightActualSlash>();
+            Item.channel = true;
+            Item.UseSound = SoundID.Item60;
+        }
+
+        public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
+        {
+            position = player.Center;
+            type = ModContent.ProjectileType<NightActualSlash>();
+            damage = Item.damage / 2;
+            knockback = Item.knockBack / 2;
+        }
+
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            if (Main.myPlayer == player.whoAmI)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Projectile.NewProjectile(source, position + new Vector2(Main.rand.Next(-150, 150), Main.rand.Next(-170, 170)), velocity, type, damage, knockback);
+                }
+            }
+            return false;
+        }
+
+        public override void AddRecipes()
+        {
+            Recipe recipe1 = CreateRecipe();
+            recipe1.AddIngredient(ModContent.ItemType<Parashard>(), 20);
+            recipe1.AddIngredient(ItemID.Katana, 1);
+            recipe1.AddTile(TileID.Anvils);
+            recipe1.Register();
+        }
+    }
+}
---
+++ Content/Projectiles/NightActualSlash.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Paracosm.Content.Items.Weapons;
+using Terraria;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Paracosm.Content.Projectiles
+{
+    public class NightActualSlash : ModProjectile
+    {
+        ref float AITimer => ref Projectile.ai[0];
+        ref float randomValue => ref Projectile.ai[1];
+        ref float randomValue2 => ref Projectile.ai[2];
+        bool released = false;
+
+        public override void SetStaticDefaults()
+        {
+            Main.projFrames[Projectile.type] = 4;
+        }
+        public override void SetDefaults()
+        {
+            Projectile.owner = Main.myPlayer;
+            Projectile.width = 64;
+            Projectile.height = 64;
+            Projectile.timeLeft = 200;
+            Projectile.aiStyle = -1;
+            Projectile.friendly = true;
+            Projectile.penetrate = -1;
+            Projectile.DamageType = DamageClass.Melee;
+            Projectile.tileCollide = false;
+            Projectile.alpha = 0;
+            Projectile.usesLocalNPCImmunity = true;
+            Projectile.localNPCHitCooldown = 30;
+        }
+
+        public override void AI()
+        {
+
+            if (Projectile.timeLeft == 200 && Main.netMode != NetmodeID.MultiplayerClient)
+            {
+                randomValue2 = Main.rand.Next(30, 60);
+                Projectile.netUpdate = true;
+            }
+            if (Projectile.timeLeft > 200 - randomValue2)
+            {
+                Projectile.alpha = 255;
+                Projectile.damage = 0;
+                return;
+            }
+
+            if (Projectile.timeLeft == 200 - randomValue2)
+            {
+                Projectile.alpha = 0;
+                if (Main.netMode != NetmodeID.MultiplayerClient)
+                {
+                    randomValue = Main.rand.Next(-180, 180);
+                    Projectile.rotation = MathHelper.ToRadians(randomValue);
+                    Projectile.netUpdate = true;
+                }
+                SoundEngine.PlaySound(SoundID.Item60 with { MaxInstances = 2 });
+                Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.Ash, -Projectile.velocity.X, -Projectile.velocity.Y);
+            }
+            Projectile.damage = 30;
+            int frameDur = 4;
+            AITimer++;
+            Player player = Main.player[Projectile.owner];
+            Projectile.frameCounter++;
+            if (Projectile.frameCounter == frameDur)
+            {
+                Projectile.frameCounter = 0;
+                Projectile.frame++;
+                if (Projectile.frame == 4)
+                {
+                    Projectile.Kill();
+                }
+            }
+        }
+    }
+}
